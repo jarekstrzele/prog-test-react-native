3c6ea08e772b0f6a9465f0da3783f8429428ae01
jarek <js100code@gmail.com>
2023-03-08 07:06:23 +0100

    0803

--- ClockApp/ClockApp.jsx
+++ ClockApp/ClockApp.jsx
@@ -1,36 +1,84 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Moment from 'react-moment';
-import moment from 'moment';
+import { StyleSheet, Text, View } from 'react-native';
 
-const ClockApp = () => {
-  const [time, setTime] = useState(moment());
+export default function App() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
 
   useEffect(() => {
-    const interval = setInterval(() => setTime(moment()), 1000);
-    return () => clearInterval(interval);
+    const intervalId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        <Moment element={Text} format="HH:mm:ss">{time}</Moment>
-      </Text>
+      <Text style={styles.clock}>{time}</Text>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-    , justifyContent: 'center'
-    , alignItems: 'center'
-    , backgroundColor: '#999'
-    ,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  text: {
-    fontSize: 50,
+  clock: {
+    fontSize: 48,
+    fontWeight: 'bold',
   },
 });
 
-export default ClockApp;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
+// import Moment from 'react-moment';
+// import moment from 'moment';
+
+// const ClockApp = () => {
+//   const [time, setTime] = useState(moment());
+
+//   useEffect(() => {
+//     const interval = setInterval(() => setTime(moment()), 1000);
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>
+//         <Moment element={Text} format="HH:mm:ss">{time}</Moment>
+//       </Text>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1
+//     , justifyContent: 'center'
+//     , alignItems: 'center'
+//     , backgroundColor: '#999'
+//     ,
+//   },
+//   text: {
+//     fontSize: 50,
+//   },
+// });
+
+// export default ClockApp;
